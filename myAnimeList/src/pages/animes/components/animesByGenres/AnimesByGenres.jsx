c3e65d3005e5f3d9493c320e8a987455238834fc
Anderson Willian <anderson2017.willian@gmail.com>
2025-02-17 21:00:10 -0300

    diretorio

--- myAnimeList/src/pages/animes/components/animesByGenres/AnimesByGenres.jsx
+++ myAnimeList/src/pages/animes/components/animesByGenres/AnimesByGenres.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Collection from "../Collection";
-import Sidebar from "../layout/sideBar.jsx";
+import Sidebar from "../layout/Sidebar.jsx";
 import Pagination from "../Pagination";
 import Loading from "../Loading";
 
