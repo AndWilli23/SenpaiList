db722085a4f09d5a1d81fb2568ed27dfbd47f011
Anderson Willian <anderson2017.willian@gmail.com>
2025-03-27 22:17:30 -0300

    Ajuste das rotas com HashRouter

--- myAnimeList/src/main.jsx
+++ myAnimeList/src/main.jsx
@@ -1,9 +1,9 @@
+import {HashRouter as BrowserRouter, Routes, Route } from 'react-router-dom';  
 import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import "bootstrap-icons/font/bootstrap-icons.css";
 import Home from './pages/animes/components/Home.jsx'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';  
 import DetailsAnimes from './pages/animes/components/detailsAnimes/DetailsAnimes.jsx';
 import HomeSearch from './pages/animes/components/searchAnimes/HomeSearch.jsx';
 import AnimesByGenres from './pages/animes/components/animesByGenres/AnimesByGenres.jsx';
